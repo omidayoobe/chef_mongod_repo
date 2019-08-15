469c074a34ed7d72fa18cd6d9db93deafbb91b09
Omid Ayoobe <OAyoobe@spartaglobal.com>
2019-08-15 10:11:02 +0100

    solved mongod problem

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -14,3 +14,14 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+
+describe package "mongodb-org" do
+  it { should be_installed }
+  its('version') { should match /3\./}
+end
+
+describe service "mongod" do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running}
+end
